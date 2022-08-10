22ca6e41703a69fe5e2e30454f698802bb847545
j0rdan0 <georgepaunesco@gmail.com>
2022-08-10 10:02:46 +0300

    modified a bit pgrep-ng

--- pgrep-ng/main.c
+++ pgrep-ng/main.c
@@ -5,34 +5,31 @@
 #include <string.h>
 #define SIZE 256
 
-int main(int argc, char* argv[]) {
+void getCmdline(char* pid) {
+        char* path = malloc(256);
+        sprintf(path,"/proc/%s/cmdline",pid);
 
-if(argc < 2) {
-        printf("Usage: %s <PID>\n",argv[0]);
-        return -1;
-}
-
-char* path = malloc(256);
-sprintf(path,"/proc/%s/cmdline",argv[1]);
-
-FILE* f = fopen(path,"r");
-if (!f) {
-        if( errno == 2) // file not found
-                printf("Invalid PID\n"); // most likely process PID incorrect or process no longer runs
-        return -1;
-}
+        FILE* f = fopen(path,"r");
+        if (!f) {
+                if( errno == 2) {// file not found
+                        printf("Invalid PID\n");  // most likely process PID incorrect or process no longer runs        
+                }  
+                free(path);     
+                return;
+        }
         free(path);
+
         char* comm = malloc(SIZE);
         fread(comm,SIZE,1,f);
 
         if ( strlen(comm) == 0) {
                 char *statPath = malloc(256);
-                sprintf(statPath,"/proc/%s/stat",argv[1]);
+                sprintf(statPath,"/proc/%s/stat",pid); // use /proc/$PID/stat for kernel threads to fetch comm
 
-                FILE* f2 = fopen(statPath,"r");
+                FILE* f = fopen(statPath,"r");
                 free(statPath);
                 char* comm = malloc(SIZE);
-                fread(comm,SIZE,1,f2);
+                fread(comm,SIZE,1,f);
                 int start; // need to get start of cmdline
                 for(int i = 0; i < strlen(comm);i++) {
                         if(comm[i] == '(') {
@@ -44,12 +41,22 @@ if (!f) {
                 }
                 printf("\n");
                 free(comm);
-                fclose(f2);
-                return 0;
+                fclose(f);
+                
         }
         printf("%s\n",comm);
         free(comm); 
         fclose(f);
+}
+
+int main(int argc, char* argv[]) {
+
+        if(argc < 2) {
+                printf("Usage: %s <PID>\n",argv[0]);
+        return -1;
+        }
+
+getCmdline(argv[1]);
 return 0;
 }
 
